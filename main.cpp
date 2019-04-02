18c32aab74de5a53de0f6ee56651ddbe4ab78318
DanielUtec123 <34191864+DanielUtec123@users.noreply.github.com>
2019-04-02 15:15:15 -0500

    accepted solution

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-	return EXIT_SUCCESS;
-}
 
+int main() {
+    int T;
+    cin >> T;
+
+
+    for (int i=0;i<T;i++) {
+        int N;
+        cin >> N;
+        int first,second,number,high = 0,low = 0;
+
+        cin >> first;
+        second = first;
+        for (int j=1;j<N;j++)
+        {
+            cin >> number;
+
+            first = second;
+
+            second = number;
+
+            if (first > second){
+                low ++;
+            }
+            if(second > first){
+                high ++;
+            }
+
+
+        }
+
+        cout << "Case " << i+1<<": "<<high << " " <<low << endl;
+    }
+
+    return 0;
+}
